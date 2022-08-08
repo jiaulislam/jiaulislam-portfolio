f652fbc9cebb2560b8ad48be15e4d48331d0940e
Jiaul Islam <jiaulislam.ict.bd@gmail.com>
2022-08-08 18:09:42 +0600

    build: latest react 18 root style changed & test if switched off now on commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "app/app";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = createRoot(document.querySelector("#root"));
+root.render(<App />);
