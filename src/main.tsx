725836d57104b1ef3d989cbed86e453772820e70
jiaulislam <kmjiaulislam@gmail.com>
2022-08-09 09:36:17 +0600

    build: added type for root element

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { createRoot } from "react-dom/client";
+import { createRoot, Root } from "react-dom/client";
 import "./index.css";
 import App from "app/app";
 
-const root = createRoot(document.querySelector("#root"));
+const root: Root = createRoot(document.querySelector("#root"));
 root.render(<App />);
